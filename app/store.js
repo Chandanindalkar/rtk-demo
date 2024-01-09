06c3c1f71f6fe24d40b5673c7c3d4a37b44db90d
Chandanindalkar <chandan.s.indalkar@gmail.com>
2024-01-09 11:07:16 +0530

    added async logic using asyncThunk

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const userReducer = require('../features/user/userSlice');
 
 const reduxLogger = require('redux-logger');
 const logger = reduxLogger.createLogger();
@@ -9,10 +10,11 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
+        user: userReducer,
     },
     // by default the configureStore adds some middleware to the redux store automatically
     // so to the list of default middlewares, we add our logger
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 module.exports = store;
---
+++ features/user/userSlice.js
@@ -0,0 +1,43 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk;
+const axios = require('axios');
+
+
+const initialState = {
+    loading: true,
+    users: [],
+    error: '',
+}
+
+// asyncThunk params
+// @firstParam = action type
+// @secondParam = callback function that has async logic
+// returns action creator functions
+const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
+    return axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.data.map(user => user.name)) // manipulate this
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.loading = false
+            state.users = action.payload
+            state.error = ''
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.loading = false
+            state.users = []
+            state.error = action.error.message
+        })
+    },
+});
+
+module.exports = userSlice.reducer;
+module.exports.fetchUsers = fetchUsers;
